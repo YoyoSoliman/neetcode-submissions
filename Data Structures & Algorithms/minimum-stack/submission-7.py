eca48b2602dc22c0863173b137d69ef38d91f2cd
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-07-11 20:03:54 +0300

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,35 @@
+class MinStack:
+    def __init__(self):
+        self.l=[]
+        self.minStack = []
+        self.size=-1
+
+    def push(self, val: int) -> None:
+
+        self.l.append(val)
+        if self.size < 0:
+            self.minStack.append(val)
+        else: 
+            if val < self.minStack[self.size]:
+                self.minStack.append(val)
+            else:
+                self.minStack.append(self.minStack[self.size])
+
+        print("Val: " + str(val))
+        print("Min: " + str(self.l[self.size]))
+        print(self.minStack)
+        self.size+=1
+
+    def pop(self) -> None:
+        if self.size >= 0:
+            del self.l[self.size]
+            del self.minStack[self.size]
+            self.size-=1
+
+    def top(self) -> int:
+        if self.size >=0: 
+            return self.l[self.size] 
+
+    def getMin(self) -> int:
+        return self.minStack[self.size]
+
