2d8d746380cfdd482c1bbd6adce3ac4d2487bf39
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-07-16 00:28:59 +0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,19 @@
+#include <unordered_set>
+#include <string>
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        std::unordered_set<int> seen;
+
+        for (int i = 0; i < nums.size();i++) {
+            if (seen.contains(nums[i])) {
+                return true;
+            }
+            seen.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
