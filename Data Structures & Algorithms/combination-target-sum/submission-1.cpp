c8cf32c2740adff0b4f1a6df67178fa607f11597
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-07-18 17:53:48 +0300

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+    vector<vector<int>> res;
+    vector<int> path;
+
+
+public:
+    void backTrack(vector<int>& nums, int index, vector<int>& path,int currSum,int target) {
+
+        //when to add the current path as a valid result
+        if (currSum == target) {
+            res.push_back(path);
+            return;
+        }
+
+        //base case(when we know we are done) if current sum exceeds the target or we went trhough all the numbers
+        if (index >= nums.size() || currSum > target) {
+            return;
+        }
+
+        //option 1: stay on current index and add it to path
+        path.push_back(nums[index]);
+        backTrack(nums,index,path,currSum + nums[index], target);
+
+        //option 2: dont add it and move on
+        path.pop_back();
+        backTrack(nums,index + 1,path,currSum, target);
+
+
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        backTrack(nums,0,path,0,target);
+        return res;
+    }
+};
