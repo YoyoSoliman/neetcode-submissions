a43fdc6adbd6c2c3a01112d68a912a6e2b276d0e
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-07-23 01:54:14 +0300

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0;
+       for (int y = 0; y < grid.size(); y++) {
+            for (int x = 0; x < grid[0].size();x++) {
+                if (grid[y][x] == '1') {
+                    grid[y][x] = 'A';
+                    res++;
+                    dfs(grid,x,y);
+                }
+
+            }
+
+       } 
+
+       return res;
+    }
+
+    void dfs(vector<vector<char>>& grid,int x, int y) {
+
+        //go right
+        if (x + 1 < grid[0].size() && grid[y][x+1] == '1') {
+            grid[y][x+1] = 'A';
+            dfs(grid,x+1,y);
+        }
+
+        //go up
+        if (y-1 >= 0 && grid[y-1][x] == '1') {
+            grid[y-1][x] = 'A';
+            dfs(grid,x,y-1);
+        }
+
+        //go left
+        if(x-1 >= 0 && grid[y][x-1] == '1') {
+            grid[y][x-1] = 'A';
+            dfs(grid,x-1,y);
+        }
+
+        //go down
+        if (y+1 < grid.size() && grid[y+1][x] == '1'){
+            grid[y+1][x] = 'A';
+            dfs(grid,x,y+1);
+
+        }
+    }
+};
