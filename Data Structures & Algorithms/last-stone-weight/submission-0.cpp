2ae4ca12d0a57f1ff13c89f181de05758f151161
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-07-23 01:28:44 +0300

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        std::priority_queue<int> maxHeap;
+        for (int i = 0; i < stones.size();i++) {
+            maxHeap.push(stones[i]);
+        }
+
+        while (maxHeap.size() > 1) {
+            int x = maxHeap.top();
+            maxHeap.pop();
+            int y = maxHeap.top();
+            maxHeap.pop();
+
+            if (x < y) {
+                maxHeap.push(y-x);
+            }
+
+            if (x>y) {
+                maxHeap.push(x-y);
+            }
+
+        }
+
+        if (maxHeap.size() == 0) {
+            return 0;
+        } else {
+            return maxHeap.top();
+        }
+    }
+};
