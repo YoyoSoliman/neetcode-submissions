ec7337a5a873fc45b1968753db9752fd4d78da53
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-07-31 01:00:16 +0300

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    std::unordered_set<int> visited;
+    std::unordered_map<int,vector<int>> connections;
+    int count = 0;
+
+    bool dfs(int curr, int parent) {
+        count++;
+        if (visited.contains(curr)) {
+            return false;
+        }
+
+        visited.insert(curr);
+
+        for (int child: connections[curr]) {
+            if (child != parent) {
+                if (!dfs(child,curr)) {
+                    return false;
+                }
+            }
+        }
+
+
+        return true;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.empty()){
+            return true;
+        }
+        for (const auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            connections[u].push_back(v);
+            connections[v].push_back(u);
+        }
+
+        if(!dfs(0,-1)) {
+            return false;
+        }
+        return visited.size() == n;
+        
+
+    }
+};
