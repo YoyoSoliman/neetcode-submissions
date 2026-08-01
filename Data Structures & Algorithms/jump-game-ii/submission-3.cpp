aa01880e660893c15babcfb67515772c4481011e
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-08-01 23:37:22 +0300

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int jumps = 0;
+        int l = 0;
+        int r = 0;
+
+        while (r < nums.size()-1) {
+            int farthest = 0;
+            for (int i = l; i < r+1;i++){
+                farthest = max(farthest, i+nums[i]);
+            }
+            l = r+1;
+            r=farthest;
+            jumps+=1;
+
+        }
+
+        return jumps;
+    }
+};
