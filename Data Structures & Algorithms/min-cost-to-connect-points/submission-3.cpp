a9de3e79e4bf62a19bbbc07480f883a6e48591a5
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-08-26 10:07:06 -0700

    Add: min-cost-to-connect-points - submission-3

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+
+    void getConnectedPoints(int node,vector<vector<int>>& points) {
+        for (int i = 0; i < points.size();i++) {
+            if (i!= node) {
+                int mD = abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1]);
+                minHeap.push({mD,i});
+            }
+        }
+
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        unordered_set<int> visited;
+
+        visited.insert(0);
+        getConnectedPoints(0,points);
+
+        int sum = 0;
+        int edges = 0;
+
+        while (edges != points.size()-1 && !minHeap.empty()) {
+            int cost = minHeap.top().first;
+            int node = minHeap.top().second; 
+            minHeap.pop();
+
+            if (!visited.count(node)) {
+                visited.insert(node);
+                getConnectedPoints(node,points);
+                edges++;
+                sum += cost;
+            }
+        }
+
+        return sum;
+    }
+};
