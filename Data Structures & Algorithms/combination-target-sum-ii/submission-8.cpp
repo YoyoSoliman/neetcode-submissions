ff437b01a5512fcbc3bd190882df34b3b9346dc4
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-09-16 17:09:14 -0700

    Add: combination-target-sum-ii - submission-8

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-8.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+
+    void backtrack(int index, int currSum,int target, vector<int>& path,const vector<int>& candidates){
+
+        if (currSum == target) {
+            res.push_back(path);
+            return;
+        }
+
+        if (currSum > target || index >= candidates.size()) {
+            return;
+        }
+
+        //choice 1 : add number and move on to next value
+        path.push_back(candidates[index]);
+        backtrack(index+1,currSum + candidates[index], target,path,candidates);
+        path.pop_back();
+
+        //choice 2: don't add umber and mobve to next Uniue number;
+        while (index + 1 < candidates.size() && candidates[index] == candidates[index+1]) {
+            index++;
+        }
+
+        backtrack(index+1,currSum, target,path,candidates);
+
+
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        std::sort(candidates.begin(),candidates.end());
+        vector<int> p = {};
+        backtrack(0,0,target,p,candidates);
+
+        return res;
+
+
+    }
+};
