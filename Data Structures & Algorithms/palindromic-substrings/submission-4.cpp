9211232d269ed93fec04343809848324da255693
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-09-22 12:32:46 -0700

    Add: palindromic-substrings - submission-4

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-4.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int count = 0;
+
+        for (int i = 0; i < s.size();i++) {
+            int l = i;
+            int r = i;
+
+            while (l >= 0 and r < s.size()) {
+                if (s[l] == s[r]) {
+                    count++;
+                    l--;
+                    r++;
+                } else {
+                    break;
+                }
+            }
+
+            l = i;
+            r = i+1;
+
+            while (l >= 0 and r < s.size()) {
+                if (s[l] == s[r]) {
+                    count++;
+                    l--;
+                    r++;
+                } else {
+                    break;
+                }
+            }
+
+
+        }
+
+        return count;
+    }
+};
