b5a3a30bdbca93d6a28e0d34936087db0ea77338
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-09-24 09:58:56 -0700

    Add: course-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-8.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> states;
+    vector<int> res;
+    bool loop = false;
+    unordered_map<int,vector<int>> adjList;
+
+    void dfs(int curr) {
+
+        if (states[curr] == 2 || loop) {
+            return;
+        }
+        if (states[curr] == 1) {
+            loop = true;
+            return;
+        } 
+
+        states[curr] = 1;
+
+        for (int n : adjList[curr]) {
+            dfs(n);
+        }
+
+        states[curr] = 2;
+        res.push_back(curr);
+
+
+
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        states.assign(numCourses,0);
+
+        for (vector<int> prereq : prerequisites) {
+            adjList[prereq[1]].push_back(prereq[0]);
+        }
+
+        for (int i = 0 ; i < numCourses;i++) {
+            dfs(i);
+        }
+
+        if (loop) {
+            return {};
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
