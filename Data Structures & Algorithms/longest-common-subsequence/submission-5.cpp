0947661f3672b3b7060d78185ac28c3617a2e2c5
Yoyo Soliman <151338070+YoyoSoliman@users.noreply.github.com>
2026-09-26 10:21:51 -0700

    Add: longest-common-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int dfs(int i,int j,const string& text1, const string& text2) {
+        if (i >= text1.size() || j >= text2.size()) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int choice1=0;
+        if (text1[i] == text2[j]) {
+            choice1 = 1 + dfs(i+1,j+1,text1,text2);
+        }
+
+        int choice2 = dfs(i+1,j,text1,text2);
+
+        int choice3 = dfs(i,j+1,text1,text2);
+
+        dp[i][j] = max({choice1,choice2,choice3});
+
+        return dp[i][j];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        dp.assign(text1.size(),vector<int>(text2.size(),-1));
+
+        return dfs(0,0,text1,text2);
+    }
+};
